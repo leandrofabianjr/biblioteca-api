ec5905f2efd40a33b45d051f5d383246ba383f2e
leandrofabianjr <leandrofabianjr@gmail.com>
2022-04-07 16:17:50 -0300

    Script de build

--- build-dist-windows.ps1
+++ build-dist-windows.ps1
@@ -1,11 +1,39 @@
+# Requisitos
+# - PowerShell, NPM, NestJS, Angular e Git
+# - biblioteca-front e biblioteca-api no mesmo diretório
+#
+# Será gerado o diretório biblioteca-dist no nível do diretório dos projetos
+
+#
+$DIST_DIR = "biblioteca-dist"
+$DIST_DIR_PATH = "..\$($DIST_DIR)\"
+$FRONT_DIR = "biblioteca-front"
+$FRONT_DIR_PATH = "..\$($FRONT_DIR)\"
+$API_DIR = "biblioteca-api"
+$API_DIR_PATH = "..\$($API_DIR)\"
+
+if (Test-Path $DIST_DIR_PATH) {
+    Remove-Item $DIST_DIR_PATH -Recurse -Force
+}
+New-Item -Path ".." -Name $DIST_DIR -ItemType "directory"
+
 # Build da API
+npm install
 nest build
-Remove-Item "..\biblioteca-dist\" -Recurse -Force -Verbose
-New-Item -Path ".." -Name "biblioteca-dist" -ItemType "directory"
-Copy-Item -Path ".\dist\" -Destination "..\biblioteca-dist\" -PassThru -Recurse
-Copy-Item -Path ".\package.json" -Destination "..\biblioteca-dist\" -PassThru
+Copy-Item -Path ".\dist\" -Destination "$($DIST_DIR_PATH)\api" -PassThru -Recurse
+Copy-Item -Path ".\package.json" -Destination $DIST_DIR_PATH -PassThru
 
 # Build do front-end
-Set-Location "..\biblioteca-front\"
+Set-Location $FRONT_DIR_PATH
+npm install
 ng build
-Copy-Item -Path ".\dist\biblioteca" -Destination "..\biblioteca-dist\front" -PassThru -Recurse
+Copy-Item -Path ".\dist\biblioteca" -Destination "$($DIST_DIR_PATH)\front" -PassThru -Recurse
+
+# # Subindo para o github
+Set-Location $DIST_DIR_PATH
+# git init
+# git add .
+# git commit -m "Commit gerado por script"
+# git remote add origin https://github.com/leandrofabianjr/biblioteca-dist.git
+# git branch -M main
+# git push -u origin main
