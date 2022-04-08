2671a4089cef329f9fb1d694eaa3e8129783fa49
leandrofabianjr <leandrofabianjr@gmail.com>
2022-04-08 17:48:53 -0300

    Correções na configuração de acesso ao banco de dados

--- src/db.config.ts
+++ src/db.config.ts
@@ -1,23 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const DB_CONFIG: TypeOrmModuleOptions = process.env.DEBUG
-  ? {
-      type: 'sqlite',
-      database: 'db.sqlite3',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }
-  : {
-      type: 'postgres',
-      synchronize: process.env.DB_SYNC == 'true',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_USER_PASS,
-      database: process.env.DB_NAME,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-    };
+function buildDbConfig(): TypeOrmModuleOptions {
+  const generalConfig = {
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: process.env.DB_SYNC == 'true',
+  };
+
+  return process.env.DB_SQLITE == 'true'
+    ? {
+        type: 'sqlite',
+        database: 'db.sqlite3',
+        ...generalConfig,
+      }
+    : {
+        type: 'postgres',
+        url: process.env.DB_POSTGRES_URL,
+        ssl: true,
+        extra: { ssl: { rejectUnauthorized: false } },
+        ...generalConfig,
+      };
+}
+
+export const DB_CONFIG = buildDbConfig();
